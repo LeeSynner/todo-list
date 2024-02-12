1838170a954cd7509ac8793274dc3f9c3ef8568d
LeeSynner <lee.synner.yu@gmail.com>
2024-02-12 21:37:38 +0500

    Added migrations and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory()->create([
+             'name' => 'Test User',
+             'email' => 'test@example.com',
+             'password' => '123456'
+         ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User2',
+            'email' => 'test2@example.com',
+            'password' => '123456'
+        ]);
+
+//        \App\Models\User::factory(10)->create();
+
+        $this->call([
+            StatusSeeder::class,
+            TaskSeeder::class
+        ]);
     }
 }
