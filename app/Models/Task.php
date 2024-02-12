af378b0a470e449294fb2ab219c335f200dc0d72
LeeSynner <lee.synner.yu@gmail.com>
2024-02-12 21:38:51 +0500

    Added Models for statuses and tasks tables

---
+++ app/Models/Task.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Task extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $guarded = [];
+
+    public function status(): BelongsTo
+    {
+        return $this->BelongsTo(Status::class, 'status_id');
+    }
+}
