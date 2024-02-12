66e85d08e56e588f7adfcb1678db521c4a9fcccb
LeeSynner <lee.synner.yu@gmail.com>
2024-02-12 21:49:04 +0500

    Added routes for Todo and TodoApi

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/token', [LoginController::class, 'token'])->name('api.login');
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/tasks', [TaskController::class, 'index']   )->name('api.tasks.index');
+    Route::post('/tasks', [TaskController::class, 'store']   )->name('api.tasks.store');
+    Route::get('/tasks/{id}', [TaskController::class, 'show'])->name('api.tasks.show');
+    Route::put('/tasks/{id}', [TaskController::class, 'update']   )->name('api.tasks.update');
+    Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->name('api.tasks.destroy');
+
 });
